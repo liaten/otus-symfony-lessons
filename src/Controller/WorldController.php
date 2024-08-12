69ddfe644d0fedc8cad44e266dd4805f37887649
Vladislav Pesterev <vlad171vlad@gmail.com>
2024-08-12 06:19:14 +0300

    Add formatters, apply cite_formatter

--- src/Controller/WorldController.php
+++ src/Controller/WorldController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Domain\Service\FormatService;
 use App\Domain\Service\GreeterService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,15 +12,20 @@ use Symfony\Component\HttpFoundation\Response;
 class WorldController extends AbstractController
 {
 
-    public function __construct(private readonly GreeterService $greeterService)
+    public function __construct(
+        private readonly FormatService  $formatService,
+        private readonly GreeterService $greeterService
+    )
     {
     }
 
     public function hello(): Response
     {
+        $result = $this->formatService->format($this->greeterService->greet('Jesus Christ'));
         return new Response(
             sprintf("<html lang=\"RU\"><body>%s</body></html>",
-                $this->greeterService->greet('world'))
+                $result
+            )
         );
     }
 
