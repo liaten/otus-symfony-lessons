86cad5572b064db9432216cd3cc74b1c0325de36
Vladislav Pesterev <vlad171vlad@gmail.com>
2024-08-20 21:02:20 +0300

    Final lesson 3

--- src/Application/Symfony/FormatterPass.php
+++ src/Application/Symfony/FormatterPass.php
@@ -9,7 +9,7 @@ use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
 
-class GreeterPass implements CompilerPassInterface
+class FormatterPass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container): void
     {
@@ -17,10 +17,9 @@ class GreeterPass implements CompilerPassInterface
             return;
         }
         $messageService = $container->findDefinition(MessageService::class);
-        $greeterServices = $container->findTaggedServiceIds('app.greeter_service');
-        uasort($greeterServices, static fn(array $tag1, array $tag2) => $tag1[0]['priority'] - $tag2[0]['priority']);
-        foreach ($greeterServices as $id => $tags) {
-            $messageService->addMethodCall('addGreeter', [new Reference($id)]);
+        $formatterServices = $container->findTaggedServiceIds('app.formatter_service');
+        foreach ($formatterServices as $id => $tags) {
+            $messageService->addMethodCall('addFormatter', [new Reference($id)]);
         }
     }
 }
