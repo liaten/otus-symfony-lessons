94506df57ce451a1ee6f8acb1c43e6106af3dc08
Vladislav Pesterev <vlad171vlad@gmail.com>
2024-08-23 21:14:52 +0300

    https://github.com/otusteamedu/symfony-course-2024-07/blob/lesson-5/docs/04_Doctrine_ORM.md#добавляем-сохранение-пользователя-в-бд

--- src/Controller/WorldController.php
+++ src/Controller/WorldController.php
@@ -4,29 +4,22 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use App\Domain\Service\FormatService;
-use App\Domain\Service\MessageService;
+use App\Domain\Service\UserService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 class WorldController extends AbstractController
 {
-
     public function __construct(
-        private readonly FormatService  $formatService,
-        private readonly MessageService $messageService
+        private readonly UserService $userService,
     )
     {
     }
 
     public function hello(): Response
     {
-        $result = $this->formatService->format($this->messageService->printMessages('Jesus Christ'));
-        return new Response(
-            sprintf("<html lang=\"RU\"><body>%s</body></html>",
-                $result
-            )
-        );
-    }
+        $user = $this->userService->create('My user');
 
+        return $this->json($user->toArray());
+    }
 }
---
+++ src/Domain/Service/UserService.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Service;
+
+use App\Domain\Entity\User;
+
+class UserService
+{
+    public function create(string $login): User
+    {
+        $user = new User();
+        $user->setLogin($login);
+        $user->setCreatedAt();
+        $user->setUpdatedAt();
+
+        return $user;
+    }
+}
