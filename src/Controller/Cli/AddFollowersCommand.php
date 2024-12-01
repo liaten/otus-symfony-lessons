a06c9f117f7b91db716c855785307ca6a0233674
Vladislav Pesterev <vlad171vlad@gmail.com>
2024-12-01 20:25:11 +0300

    Lesson 21
    https://github.com/otusteamedu/symfony-course-2024-07/blob/lesson-21/docs/21_Console_commands.md#%D0%B4%D0%BE%D0%B1%D0%B0%D0%B2%D0%BB%D1%8F%D0%B5%D0%BC-%D0%BF%D0%BE%D0%B4%D0%BF%D0%B8%D1%81%D0%BA%D1%83-%D0%BD%D0%B0-%D1%81%D0%BE%D0%B1%D1%8B%D1%82%D0%B8%D0%B5-%D0%B7%D0%B0%D0%BF%D1%83%D1%81%D0%BA%D0%B0-%D0%BA%D0%BE%D0%BC%D0%B0%D0%BD%D0%B4%D1%8B

--- src/Controller/Cli/AddFollowersCommand.php
+++ src/Controller/Cli/AddFollowersCommand.php
@@ -6,45 +6,65 @@ namespace App\Controller\Cli;
 use App\Domain\Service\FollowerService;
 use App\Domain\Service\UserService;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Command\LockableTrait;
+use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class AddFollowersCommand extends Command
+final class AddFollowersCommand extends Command
 {
+    use LockableTrait;
+
+    private const DEFAULT_FOLLOWERS = 10;
+    private const DEFAULT_LOGIN_PREFIX = 'Reader #';
+
     public function __construct(
         private readonly UserService $userService,
         private readonly FollowerService $followerService
-    )
-    {
+    ) {
         parent::__construct();
     }
 
     protected function configure(): void
     {
         $this->setName('followers:add')
+            ->setHidden()
             ->setDescription('Adds followers to author')
             ->addArgument('authorId', InputArgument::REQUIRED, 'ID of author')
-            ->addArgument('count', InputArgument::REQUIRED, 'How many followers should be added');
+            ->addArgument('count', InputArgument::OPTIONAL, 'How many followers should be added')
+            ->addOption('login', 'l', InputOption::VALUE_REQUIRED, 'Follower login prefix');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $authorId = (int)$input->getArgument('authorId');
         $user = $this->userService->findUserById($authorId);
+
         if ($user === null) {
             $output->write("<error>User with ID $authorId doesn't exist</error>\n");
             return self::FAILURE;
         }
-        $count = (int)$input->getArgument('count');
+
+        $count = (int)($input->getArgument('count') ?? self::DEFAULT_FOLLOWERS);
         if ($count < 0) {
             $output->write("<error>Count should be positive integer</error>\n");
             return self::FAILURE;
         }
 
-        $result = $this->followerService->addFollowersSync($user, "Reader #{$authorId}", $count);
+        $login = $input->getOption('login') ?? self::DEFAULT_LOGIN_PREFIX;
 
-        $output->write("<info>$result followers were created</info>\n");
+        $result = 0;
+        $progressBar = new ProgressBar($output, $count);
+        $progressBar->start();
+        for ($i = 1; $i <= $count; $i++) {
+            $result += $this->followerService->addFollowersSync($user, $login.$authorId.$i, 1);
+            usleep(200000);
+            $progressBar->advance();
+        }
+        $progressBar->finish();
+        $output->write("\n<info>$result followers were created</info>\n");
 
         return self::SUCCESS;
     }
