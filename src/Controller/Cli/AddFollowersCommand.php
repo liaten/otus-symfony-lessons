ffc47e109516fca1d5ed8fca71a10a7670211fce
Vladislav Pesterev <vlad171vlad@gmail.com>
2024-12-01 23:58:09 +0300

    Lesson 21
    Final

--- src/Controller/Cli/AddFollowersCommand.php
+++ src/Controller/Cli/AddFollowersCommand.php
@@ -7,11 +7,11 @@ use App\Domain\Service\FollowerService;
 use App\Domain\Service\UserService;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
 
 #[AsCommand(name: self::FOLLOWERS_ADD_COMMAND_NAME, description: 'Add followers to author', hidden: true)]
 final class AddFollowersCommand extends Command
@@ -24,14 +24,15 @@ final class AddFollowersCommand extends Command
     public function __construct(
         private readonly UserService $userService,
         private readonly FollowerService $followerService
-    ) {
+    )
+    {
         parent::__construct();
     }
 
     protected function configure(): void
     {
-        $this->addArgument('authorId', InputArgument::REQUIRED, 'ID of author')
-            ->addArgument('count', InputArgument::OPTIONAL, 'How many followers should be added')
+        $this->setName('followers:add')
+            ->addArgument('authorId', InputArgument::REQUIRED, 'ID of author')
             ->addOption('login', 'l', InputOption::VALUE_REQUIRED, 'Follower login prefix');
     }
 
@@ -45,7 +46,10 @@ final class AddFollowersCommand extends Command
             return self::FAILURE;
         }
 
-        $count = (int)($input->getArgument('count') ?? self::DEFAULT_FOLLOWERS);
+        $helper = $this->getHelper('question');
+        $question = new Question('How many followers you want to add?', self::DEFAULT_FOLLOWERS);
+        $count = (int)$helper->ask($input, $output, $question);
+
         if ($count < 0) {
             $output->write("<error>Count should be positive integer</error>\n");
             return self::FAILURE;
@@ -53,16 +57,10 @@ final class AddFollowersCommand extends Command
 
         $login = $input->getOption('login') ?? self::DEFAULT_LOGIN_PREFIX;
 
-        $result = 0;
-        $progressBar = new ProgressBar($output, $count);
-        $progressBar->start();
-        for ($i = 1; $i <= $count; $i++) {
-            $result += $this->followerService->addFollowersSync($user, $login.$authorId.$i, 1);
-            usleep(200000);
-            $progressBar->advance();
-        }
-        $progressBar->finish();
-        $output->write("\n<info>$result followers were created</info>\n");
+        $output->write('<info>Started</info>');
+        sleep(100);
+        $result = $this->followerService->addFollowersSync($user, $login . $authorId, $count);
+        $output->write("<info>$result followers were created</info>\n");
 
         return self::SUCCESS;
     }
